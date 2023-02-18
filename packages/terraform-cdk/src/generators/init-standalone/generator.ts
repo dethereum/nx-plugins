12eb0490468c0bd5c8b114b44f5516a72cca9fa7
haiturtle <33427880+dethereum@users.noreply.github.com>
2023-02-18 16:09:13 -0500

    feat(terraform-cdk): add a generator to make an independent terraform cdk project

---
+++ packages/terraform-cdk/src/generators/init-standalone/generator.ts
@@ -0,0 +1,116 @@
+import {
+  addProjectConfiguration,
+  formatFiles,
+  generateFiles,
+  getWorkspaceLayout,
+  names,
+  offsetFromRoot,
+  Tree,
+  updateJson,
+} from '@nrwl/devkit';
+import * as path from 'path';
+import { CdktfGeneratorSchema } from './schema';
+
+interface NormalizedSchema extends CdktfGeneratorSchema {
+  projectName: string;
+  projectRoot: string;
+  projectDirectory: string;
+  parsedTags: string[];
+}
+
+function normalizeOptions(
+  host: Tree,
+  options: CdktfGeneratorSchema
+): NormalizedSchema {
+  const name = names(options.name).fileName;
+  const projectDirectory = options.directory
+    ? `${names(options.directory).fileName}/${name}`
+    : `${getWorkspaceLayout(host).appsDir}/${name}`;
+  const projectName = name.replace(new RegExp('/', 'g'), '-');
+  const parsedTags = options.tags
+    ? options.tags.split(',').map((s) => s.trim())
+    : [];
+
+  return {
+    ...options,
+    projectName,
+    projectRoot: projectDirectory,
+    projectDirectory,
+    parsedTags,
+  };
+}
+
+function addFiles(host: Tree, options: NormalizedSchema) {
+  const templateOptions = {
+    ...options,
+    ...names(options.name),
+    projectName: options.name,
+    packageName: options.projectName,
+    offsetFromRoot: offsetFromRoot(options.projectRoot),
+    template: '',
+  };
+  generateFiles(
+    host,
+    path.join(__dirname, 'files'),
+    options.projectRoot,
+    templateOptions
+  );
+}
+
+export default async function (host: Tree, options: CdktfGeneratorSchema) {
+  const normalizedOptions = normalizeOptions(host, options);
+  addProjectConfiguration(host, normalizedOptions.projectName, {
+    root: normalizedOptions.projectRoot,
+    projectType: 'application',
+    sourceRoot: `${normalizedOptions.projectRoot}/src`,
+    targets: {
+      lint: {
+        executor: '@nrwl/linter:eslint',
+        options: {
+          lintFilePatterns: [`${normalizedOptions.projectRoot}/**/*.ts`],
+        },
+      },
+      test: {
+        executor: '@nrwl/jest:jest',
+        outputs: ['{workspaceRoot}/coverage/{projectRoot}'],
+        options: {
+          jestConfig: `${normalizedOptions.projectRoot}/jest.config.js`,
+          passWithNoTests: true,
+        },
+      },
+      deploy: {
+        executor: '@dethereum/nx-terraform-cdk:deploy',
+        options: {
+          buildTargets: [],
+        },
+      },
+      destroy: {
+        executor: '@dethereum/nx-terraform-cdk:destroy',
+      },
+      synth: {
+        executor: '@dethereum/nx-terraform-cdk:synth',
+      },
+    },
+    tags: normalizedOptions.parsedTags,
+  });
+
+  addFiles(host, normalizedOptions);
+
+  if (host.exists('tsconfig.json')) {
+    updateJson(host, 'tsconfig.json', (tsconfig) => {
+      if (tsconfig.references) {
+        tsconfig.references.push({
+          path: `./${normalizedOptions.projectRoot}`,
+        });
+
+        tsconfig.references.sort((a: { path: string }, b: { path: string }) =>
+          a.path.localeCompare(b.path)
+        );
+      }
+
+      return tsconfig;
+    });
+  }
+
+  await formatFiles(host);
+}
---
+++ packages/terraform-cdk/src/generators/init-standalone/schema.d.ts
@@ -0,0 +1,5 @@
+export interface CdktfGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
