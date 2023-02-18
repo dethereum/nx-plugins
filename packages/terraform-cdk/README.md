59f8c24f5fbfc5dc858249814d93a824ac1511a7
haiturtle <33427880+dethereum@users.noreply.github.com>
2023-02-18 12:26:49 -0500

    chore(terraform-cdk): scaffold the terraform-cdk plugin package
    ran pnpx create-nx-plugin nx-plugins --pluginName terraform-cdk and moved generated files up one
    level

---
+++ packages/terraform-cdk/README.md
@@ -0,0 +1,11 @@
+# terraform-cdk
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build terraform-cdk` to build the library.
+
+## Running unit tests
+
+Run `nx test terraform-cdk` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/terraform-cdk/src/generators/terraform-cdk/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+
+import generator from './generator';
+import { TerraformCdkGeneratorSchema } from './schema';
+
+describe('terraform-cdk generator', () => {
+  let appTree: Tree;
+  const options: TerraformCdkGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    appTree = createTreeWithEmptyWorkspace({ layout: 'apps-libs' });
+  });
+
+  it('should run successfully', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toBeDefined();
+  });
+});
