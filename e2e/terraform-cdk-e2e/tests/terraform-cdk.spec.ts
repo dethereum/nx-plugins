9ae6f51da2719dfe7e8781482a098fd7dc94d9be
haiturtle <33427880+dethereum@users.noreply.github.com>
2023-02-18 16:05:05 -0500

    test(terraform-cdk-e2e): add a basic test for init-standalone generator

--- e2e/terraform-cdk-e2e/tests/terraform-cdk.spec.ts
+++ e2e/terraform-cdk-e2e/tests/terraform-cdk.spec.ts
@@ -1,12 +1,11 @@
 import {
   checkFilesExist,
   ensureNxProject,
-  readJson,
   runNxCommandAsync,
   uniq,
 } from '@nrwl/nx-plugin/testing';
 
-describe('terraform-cdk e2e', () => {
+describe('terraform-cdk init-standalone generator', () => {
   // Setting up individual workspaces per
   // test can cause e2e runs to take a long time.
   // For this reason, we recommend each suite only
@@ -14,7 +13,10 @@ describe('terraform-cdk e2e', () => {
   // on a unique project in the workspace, such that they
   // are not dependant on one another.
   beforeAll(() => {
-    ensureNxProject('@nx-plugins/terraform-cdk', 'dist/packages/terraform-cdk');
+    ensureNxProject(
+      '@dethereum/nx-terraform-cdk',
+      'dist/packages/terraform-cdk'
+    );
   });
 
   afterAll(() => {
@@ -23,39 +25,25 @@ describe('terraform-cdk e2e', () => {
     runNxCommandAsync('reset');
   });
 
-  it('should create terraform-cdk', async () => {
+  it('generates the files needed for a working terraform cdk project', async () => {
+    const FILES = [
+      '.eslintrc.json',
+      'cdktf.json',
+      'jest.config.js',
+      'main.ts',
+      'package.json',
+      'setup.js',
+      'tsconfig.json',
+      '__tests__/main-test.ts',
+    ] as const;
+
     const project = uniq('terraform-cdk');
+    const expectedFiles = FILES.map((file) => `apps/${project}/${file}`);
+
     await runNxCommandAsync(
-      `generate @nx-plugins/terraform-cdk:terraform-cdk ${project}`
+      `generate @dethereum/nx-terraform-cdk:init-standalone ${project}`
     );
-    const result = await runNxCommandAsync(`build ${project}`);
-    expect(result.stdout).toContain('Executor ran');
+
+    expect(() => checkFilesExist(...expectedFiles)).not.toThrow();
   }, 120000);
-
-  describe('--directory', () => {
-    it('should create src in the specified directory', async () => {
-      const project = uniq('terraform-cdk');
-      await runNxCommandAsync(
-        `generate @nx-plugins/terraform-cdk:terraform-cdk ${project} --directory subdir`
-      );
-      expect(() =>
-        checkFilesExist(`libs/subdir/${project}/src/index.ts`)
-      ).not.toThrow();
-    }, 120000);
-  });
-
-  describe('--tags', () => {
-    it('should add tags to the project', async () => {
-      const projectName = uniq('terraform-cdk');
-      ensureNxProject(
-        '@nx-plugins/terraform-cdk',
-        'dist/packages/terraform-cdk'
-      );
-      await runNxCommandAsync(
-        `generate @nx-plugins/terraform-cdk:terraform-cdk ${projectName} --tags e2etag,e2ePackage`
-      );
-      const project = readJson(`libs/${projectName}/project.json`);
-      expect(project.tags).toEqual(['e2etag', 'e2ePackage']);
-    }, 120000);
-  });
 });
